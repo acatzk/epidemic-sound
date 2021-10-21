fd367a3c3b57f2e3f06f7a43db713f7f95f21126
acatzk <joshuaimalay@gmail.com>
2021-10-21 21:17:37 +0800

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Epidemic Sound Library</title>
+      </Head>
+      <h1 className="text-2xl font-bold text-indigo-600">Hello Celda mae</h1>
+    </div>
+  )
+}
+
+export default Home
