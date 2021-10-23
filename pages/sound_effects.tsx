3142b853fbf18e5e22d06aab982501d7aea0c018
acatzk <joshuaimalay@gmail.com>
2021-10-23 17:40:24 +0800

    feat: add mobile menu dropdown (#2)

--- pages/sound_effects.tsx
+++ pages/sound_effects.tsx
@@ -4,7 +4,9 @@ import React from 'react'
 const SoundEffects = () => {
   return (
     <Layout headTitle="SFX">
-      <h1>Hi this is the layout</h1>
+      <div className="mt-8 border-t border-gray-800">
+        <h1 className="mt-3 text-xl font-medium">Featured sound effects</h1>
+      </div>
     </Layout>
   )
 }
