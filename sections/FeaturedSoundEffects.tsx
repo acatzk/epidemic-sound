7327ed51e7a15eeb661f0c7cf6585df5a7b514b9
acatzk <joshuaimalay@gmail.com>
2021-10-28 21:19:57 +0800

    refactor: add dynamic slug sfx content (#9)

--- sections/FeaturedSoundEffects.tsx
+++ sections/FeaturedSoundEffects.tsx
@@ -9,8 +9,8 @@ interface FeaturedSoundEffectsProps {
 const FeaturedSoundEffects: React.FC<FeaturedSoundEffectsProps> = ({ data }) => {
   return (
     <section className="mt-8 grid grid-cols-3 lg:grid-cols-6 gap-8">
-      {data?.sound_effects.map(({ image, title, route }, i) => (
-        <Link href={route} key={i}>
+      {data?.sound_effects.map(({ id, image, title, slug }) => (
+        <Link href={`/sound-effects/${slug}`} key={id}>
           <a>
             <div className="overflow-hidden filter hover:brightness-75 transition ease-in-out duration-300">
               <Image
@@ -18,13 +18,13 @@ const FeaturedSoundEffects: React.FC<FeaturedSoundEffectsProps> = ({ data }) => 
                 width={225}
                 height={225}
                 layout="responsive"
-                alt="cartoon"
+                alt={title}
                 placeholder="blur"
                 blurDataURL={image}
                 objectFit="cover"
               />
             </div>
-            <h2 className="mt-3 text-base font-medium">{title}</h2>
+            <h2 className="mt-3 text-base font-medium text-capitalize">{title}</h2>
           </a>
         </Link>
       ))}
